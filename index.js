15e40bdd077083d5672ea51ce1d94d19a31c363a
ihabihab12 <aboubakert111@gmail.com>
2025-04-29 19:27:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
+const port = 9000;
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
 const app = express();
-const PORT = process.env.PORT || 3000;
+const db = require("mongoose");
+const requestIp = require("request-ip");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const verificarAuth = require("./utils/verifyAuthDate");
 
-app.use(cors());
+app.use(express.json());
+app.use(requestIp.mw());
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-    res.send("MTA Anti-Cheat API is running.");
-});
+connectDB = async () => {
+  // Função para ligar à base de dados
+  try {
+    await db.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Ligação MongoDB estabelecida.");
+    console.log("Database atual:" + db.connection.db.databaseName);
+  } catch (error) {
+    console.error("[ERRO] Ligação MongoDB não estabelecida:");
+    process.exit(1);
+  }
+};
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+connectDB();
+verificarAuth();
+
+// A cada dia chamar verificarAuth para desativar contas
+setInterval(verificarAuth, 86400000);
+
+
+// Rotas
+app.use("/v2/public", require("./routes/public"));
+app.use("/v2/priv8", require("./routes/private"));
+
+app.listen(port, function () {
+  console.log(`Servidor arrancado na porta ${port}!`);
 });
